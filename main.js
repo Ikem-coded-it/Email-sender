520d253ceb4a32d1e888cd83aa8797d7fb2bf84c
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2022-07-19 22:41:15 +0100

    removed email info

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
   let transporter = nodemailer.createTransport({
     service: 'hotmail',
     auth: {
-      user: 'ikemefunaonubogu@outlook.com',
-      pass: 'IDONTknow2'
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD
     }
   });
   
